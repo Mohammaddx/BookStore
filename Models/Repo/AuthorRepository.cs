624d0b85d44109e5c7db46de5a11eb8a5abf710e
mohammad ahamd <ma3571318z@gmail.com>
2020-01-30 20:00:44 +0300

    first Commit

---
+++ Models/Repo/AuthorRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStorProject.Models.Repo
+{
+    public class AuthorRepository : IBookStorRepository<Author>
+    {
+        IList<Author> authors;
+
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+                new Author
+                {
+                    Id=1, FullName="Mohammad Ahmad"
+                },
+                new Author
+                {
+                    Id=2, FullName="Islam Raslan"
+                },
+                new Author
+                {
+                    Id=3, FullName="Ammer Foqha"
+                }
+            };
+        }
+        public void Add(Author entity)
+        {
+            entity.Id = authors.Max(auth => auth.Id) + 1;
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(auth => auth.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            var author = Find(id);
+            author.FullName = newAuthor.FullName;
+        }
+    }
+}
---
+++ Models/Repo/BookRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStorProject.Models.Repo
+{
+    public class BookRepository : IBookStorRepository<Book>
+    {
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id=1,
+                    Title="C# Programming",
+                    Description="No description",
+                    ImageUrl="joker.png",
+                    Author = new Author()
+                },
+                
+                new Book
+                {
+                    Id=2,
+                    Title="Java Programming",
+                    Description="No description",
+                    ImageUrl="mobile design3.png",
+                    Author = new Author()
+                },
+
+                new Book
+                {
+                    Id=3,
+                    Title="Python Programming",
+                    Description="No description",
+                    ImageUrl="simba.png",
+                    Author = new Author()
+                },
+            };
+        }
+        public void Add(Book entity)
+        {
+            entity.Id = books.Max(b => b.Id) + 1;
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            var book = Find(id);
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+            book.Author = newBook.Author;
+            book.ImageUrl = newBook.ImageUrl;
+        }
+    }
+}
